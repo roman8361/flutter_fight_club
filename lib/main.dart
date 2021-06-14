dee729db0c4828350160cf8817035f8d64b2c817
roman <torex@inbox.ru>
2021-06-14 10:41:35 +0300

    home work 02 (refactoring)

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_fight_club/fight_club_colors.dart';
 import 'package:flutter_fight_club/fight_club_images.dart';
-import 'package:flutter_fight_club/flight_club_icons.dart';
+import 'package:flutter_fight_club/fight_club_icons.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
